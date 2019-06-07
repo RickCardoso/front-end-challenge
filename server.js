2232f2f30474c9271d9790ee6b643282459f8b8f
Alan Albuquerque <alanteclog@gmail.com>
2019-06-06 21:01:00 -0300

    add cors configuration to api server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.json({
